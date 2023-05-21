ef266cce70103ee1f5b19344287102cf19c293a9
Alastair <alastair.chau@gmail.com>
2023-05-21 19:09:38 +0100

    Set up env variables and @path package

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import path from 'path'; // part of pathname shortening package
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src")}]
+    // replaces the @ with src directory so you dont need to type ../../.. etc. 
+  }
 })
