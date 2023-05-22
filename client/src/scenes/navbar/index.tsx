5cb9192b2aa184f15e381f26b3e3b81eade0a86e
Alastair <alastair.chau@gmail.com>
2023-05-22 11:36:23 +0100

    Router and Navigation: Left side of Navbar (icon and title)

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import QueryStatsIcon from "@mui/icons-material/QueryStats";
 import { Box, Typography, useTheme } from "@mui/material";
 import FlexBetween from "@/components/FlexBetween";
 
@@ -11,13 +12,22 @@ const Navbar = (props: Props) => {
   grabbing the palette object in theme.ts file
   */
   const { palette } = useTheme();
+  const [selected, setSelected] = useState("dashboard"); // determines what page we are on to highlight text
 
   return (
     <FlexBetween
       marginBottom="0.25rem"
       padding="0.5rem 0rem"
       color={palette.grey[300]}
-    ></FlexBetween>
+    >
+      {/* LEFT SIDE */}
+      <FlexBetween gap="0.75rem">
+        <QueryStatsIcon sx={{ fontSize: "28px" }} />
+        <Typography variant="h4" fontSize="16px">
+          Finance Charts
+        </Typography>
+      </FlexBetween>
+    </FlexBetween>
   );
 };
 
