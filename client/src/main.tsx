e106a71d0bc33a9db67e422803c91b032736634e
Alastair <alastair.chau@gmail.com>
2023-05-23 17:04:48 +0100

    Redux Toolkit Setup and Config: Add Redux Store and Provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App';
-import '@/index.css'
+import '@/index.css';
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { api } from "@/state/api";
 
+
+// Redux Store is the main, central bucket which stores all the states of the app
+export const store = configureStore({
+  reducer: { [api.reducerPath]: api.reducer }, 
+  middleware: (getDefault) => getDefault().concat(api.middleware), // configuration needed for api call for redux toolkit query to work
+})
+
+setupListeners(store.dispatch); // configuration needed for api call, to help redux toolkit query work
+
+
+// <Provider> component makes the Redux store available to any nested components that need to access the store
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
 )
