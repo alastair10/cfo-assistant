19b12c711fe3840daaa38aedd4ec2e25b699dcfe
Alastair <alastair.chau@gmail.com>
2023-05-24 15:39:06 +0100

    Backend installation: Set up mongoose connection with .env variables, update mongoose version for compatibility, add .gitignore

--- server/index.js
+++ server/index.js
@@ -17,6 +17,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-console.log("Hello")
+console.log("Hello");
 
 // MONGOOSE SETUP
+const PORT = process.env.PORT || 9000;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(async () => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect.`));
