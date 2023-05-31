97805d0705124544428a6cb9858ffb991cefcd42
Alastair <alastair.chau@gmail.com>
2023-05-31 17:06:59 +0100

    Products UI and API: created api GET request, defined type, confirmed data is pulling in to FE

--- client/src/scenes/dashboard/row2.tsx
+++ client/src/scenes/dashboard/row2.tsx
@@ -1,9 +1,14 @@
 import DashboardBox from '@/components/DashboardBox';
+import { useGetProductsQuery } from '@/state/api';
+import { ConstructionOutlined } from '@mui/icons-material';
 import React from 'react';
 
 type Props = {}
 
 const Row2 = (props: Props) => {
+  const { data } = useGetProductsQuery();
+  // console.log(data); // check to see if we get data back 
+
   return (
     <>
       <DashboardBox gridArea="d"></DashboardBox>
--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,17 +1,23 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { GetKpisResponse } from "./types";
+import { GetKpisResponse, GetProductsResponse } from "./types";
 
 // createApi lets us create endpoints to call backend
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
   reducerPath: "main", // just name for API call
-  tagTypes: ["Kpis"],
+  tagTypes: ["Kpis", "Products"], // this is where data is saved once a GET query is made
   endpoints: (build) => ({
+    // GET Kpis query
     getKpis: build.query<Array<GetKpisResponse>, void>({  // GetKpisResponse is the object inside our data array
-      query: () => "kpi/kpis/",
+      query: () => "kpi/kpis/", // needs to match route
       providesTags: ["Kpis"]
+    }),
+    // GET products query
+    getProducts: build.query<Array<GetProductsResponse>, void>({  // GetProductsResponse is the object inside our data array
+      query: () => "product/products/",
+      providesTags: ["Products"]
     }),
   }),
 })
 
-export const { useGetKpisQuery } = api;
+export const { useGetKpisQuery, useGetProductsQuery } = api;
