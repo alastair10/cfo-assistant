cec349c9d431ad342158feb2cf075bf97e2ccc34
Alastair <alastair.chau@gmail.com>
2023-06-01 11:02:57 +0100

    Transactions UI and API: restyle the table headers, columns, and rows

--- client/src/scenes/dashboard/row3.tsx
+++ client/src/scenes/dashboard/row3.tsx
@@ -1,21 +1,77 @@
+import BoxHeader from "@/components/BoxHeader";
 import DashboardBox from "@/components/DashboardBox";
 import {
   useGetKpisQuery,
   useGetProductsQuery,
   useGetTransactionsQuery,
 } from "@/state/api";
+import { Box, useTheme } from "@mui/material";
+import { DataGrid, GridCellParams } from "@mui/x-data-grid";
 import React from "react";
 
-const Row3 = (props: Props) => {
-  // PULL IN REQUIRED DATA
+const Row3 = () => {
+  // Pull in Theme
+  const { palette } = useTheme();
+  // Pull in required Data
   const { data: transactionData } = useGetTransactionsQuery(); // pull in the data
   // console.log(transactionData) // test to see data is pulling in 50 txns
   const { data: productData } = useGetProductsQuery(); // pull in product data
   const { data: kpiData } = useGetKpisQuery(); // pull in kpi data
 
+  // define column data for the table
+  const productColumns = [
+    {
+      field: "_id",
+      headerName: "id",
+      flex: 1,
+    },
+    {
+      field: "expense",
+      headerName: "Expense",
+      flex: 0.5,
+      renderCell: (params: GridCellParams) => `£${params.value}`,
+    },
+    {
+      field: "price",
+      headerName: "Price",
+      flex: 0.5,
+      renderCell: (params: GridCellParams) => `£${params.value}`,
+    },
+  ];
+
   return (
     <>
-      <DashboardBox gridArea="g"></DashboardBox>
+      <DashboardBox gridArea="g">
+        <BoxHeader
+          title="List of Products"
+          sideText={`${productData?.length} products`}
+        />
+        <Box
+          mt="0.5rem"
+          p="0 0.5rem"
+          height="75%"
+          sx={{
+            "& .MuiDataGrid-root": { color: palette.grey[300], border: "none" },
+            "& .MuiDataGrid-cell": {
+              borderBottom: `1px solid ${palette.grey[800]} !important`,
+            },
+            "& .MuiDataGrid-columnHeaders": {
+              borderBottom: `1px solid ${palette.grey[800]} !important`,
+            },
+            "& .MuiDataGrid-columnSeparator": {
+              visibility: "hidden"
+            },
+          }}
+        >
+          <DataGrid
+            columnHeaderHeight={25}
+            rowHeight={35}
+            hideFooter={true}
+            rows={productData || []}
+            columns={productColumns}
+          />
+        </Box>
+      </DashboardBox>
       <DashboardBox gridArea="h"></DashboardBox>
       <DashboardBox gridArea="i"></DashboardBox>
       <DashboardBox gridArea="j"></DashboardBox>
