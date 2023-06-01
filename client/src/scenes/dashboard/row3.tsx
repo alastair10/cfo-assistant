fc0a73349174dfc1fdc66fefe4847cf184df431f
Alastair <alastair.chau@gmail.com>
2023-06-01 10:31:46 +0100

    Transactions UI and API: pull in txn, kpi, and product data to use in table

--- client/src/scenes/dashboard/row3.tsx
+++ client/src/scenes/dashboard/row3.tsx
@@ -1,9 +1,18 @@
-import DashboardBox from '@/components/DashboardBox';
-import React from 'react';
-
-type Props = {}
+import DashboardBox from "@/components/DashboardBox";
+import {
+  useGetKpisQuery,
+  useGetProductsQuery,
+  useGetTransactionsQuery,
+} from "@/state/api";
+import React from "react";
 
 const Row3 = (props: Props) => {
+  // PULL IN REQUIRED DATA
+  const { data: transactionData } = useGetTransactionsQuery(); // pull in the data
+  // console.log(transactionData) // test to see data is pulling in 50 txns
+  const { data: productData } = useGetProductsQuery(); // pull in product data
+  const { data: kpiData } = useGetKpisQuery(); // pull in kpi data
+
   return (
     <>
       <DashboardBox gridArea="g"></DashboardBox>
@@ -11,7 +20,7 @@ const Row3 = (props: Props) => {
       <DashboardBox gridArea="i"></DashboardBox>
       <DashboardBox gridArea="j"></DashboardBox>
     </>
-  )
-}
+  );
+};
 
 export default Row3;
